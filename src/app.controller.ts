bb3a543f49df2cc72f574b36873eafa6f5b65756
iam-4nn3 <iam.ignacia004@gmail.com>
2023-02-05 22:10:17 +0100

    my 2nd commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('home')
+  home(): string {
+    return this.appService.home();
+  }
+  @Get('CV')
+  CV(): string {
+    return this.appService.CV();
+  }
+  @Get('hello')
+  getHello3(): string {
     return this.appService.getHello();
   }
+  @Get('hello2')
+  @Render('index.html')
+  getHello2(): {} {
+    return this.appService.getHello2();
+  } 
+  @Get()
+  @Render('home.html')
+  getHome(): {} {
+    return this.appService.getHome();
+    
+  }
+  @Get('about-us')
+  @Render('about-us.html')
+  getAboutUs(): {} {
+    return this.appService.getAboutUs();
+  }
 }
+ 
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,24 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World from Anne!';
   }
+  getHello2(): {} {
+    return {message: 'Hello World!', title: 'My Greeting App'
+  }
+  }
+  home(): string {
+    return 'Thank you for visiting my home site'
+  }
+  CV(): string {
+    return 'This is my CV'
+  }
+  getHome(): {} {
+    return {title: 'Home Page'};
+  }
+  getAboutUs(): {} {
+    return {title: 'About Us Page'};
+  }
+
+
 }
